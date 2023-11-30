b1088fc85c31add4a66e147a4be178c0d879e618
shahzaib saleem <shahzaibsaleem665@gmail.com>
2023-12-01 00:47:06 +1100

    working on main page

---
+++ src/Components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Card.css"
+
+function Card({title, description, date}) {
+  return (
+    <div className='card'>
+        <h1>{title}</h1>
+        <div className="card__info">
+            <h3>{description}</h3>
+            <p>{date}</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Card
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import './Main.css'
+import Card from '../Components/Card'
+import Header from './Header'
 
 function Main() {
   return (
-    <div className='main'><h1>New Page</h1></div>
+    <div className='main'>
+      <Header />
+      <div className="main__container">
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+
+
+        <Card  title="New Doc" description="New document added" date="10 May 2023" />
+
+      </div>
+    </div>
   )
 }
 
