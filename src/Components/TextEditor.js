b94662516100b40a6d36ea4bbaf69c93b3fe8e6d
shahzaib saleem <shahzaibsaleem665@gmail.com>
2023-12-08 10:48:13 +1100

    Saving to firestore

--- src/Components/TextEditor.js
+++ src/Components/TextEditor.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactQuill from "react-quill";
 import EditorToolbar, { modules, formats } from "./EditorToolbar";
 import "react-quill/dist/quill.snow.css";
 import "./TextEditor.css";
 import { Button } from "@mui/material";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import  { auth,db } from "../utilities/Firebase";
 
 export const TextEditor = () => {
+  const [user] = useAuthState(auth);
   const [state, setState] = React.useState({ value: null });
+  const [docs, loading] = useCollectionData(
+    db.collection(`users/${user?.uid}/documents`).orderBy("createdAt")
+  );
+
+  useEffect(() => {
+    // Initialize the state with the content of the latest document
+    if (docs && docs.length > 0) {
+      setState({ value: docs[docs.length - 1].content });
+    }
+  }, [docs]);
 
   const handleChange = (value) => {
     setState({ value });
   };
 
+  
   const handleSavePlainText = () => {
     const { value } = state;
     if (value) {
@@ -32,6 +47,27 @@ export const TextEditor = () => {
     }
   };
 
+  const handleSave = () => {
+    const { value } = state;
+
+    // Check if a document exists
+    const existingDocument = docs && docs.length > 0 ? docs[docs.length - 1] : null;
+
+    if (existingDocument) {
+      // Update the existing document with new content
+      db.collection(`users/${user?.uid}/documents`).doc(existingDocument.id).update({
+        content: value,
+        updatedAt: new Date(),
+      });
+    } else {
+      // Create a new document in the Firestore collection
+      db.collection(`users/${user?.uid}/documents`).add({
+        content: value,
+        createdAt: new Date(),
+      });
+    }
+  };
+
   return (
     <div className="textEditor">
       <EditorToolbar />
@@ -44,7 +80,8 @@ export const TextEditor = () => {
         formats={formats}
         style={{ height: "500px" }}
       />
-      <Button onClick={handleSavePlainText}>Save as Plain Text</Button>
+      <Button onClick={handleSave}>Save to Firestore</Button>
+      <Button onClick={handleSavePlainText}>Save to Local Device</Button>
     </div>
   );
 };
