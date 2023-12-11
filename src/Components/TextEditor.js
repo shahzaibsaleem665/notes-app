47e0f6432eff1cb92f81f88612e852f2afa762cd
shahzaib saleem <shahzaibsaleem665@gmail.com>
2023-12-11 22:01:45 +1100

    sending and fetching data from firestore

--- src/Components/TextEditor.js
+++ src/Components/TextEditor.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import EditorToolbar, { modules, formats } from "./EditorToolbar";
 import "react-quill/dist/quill.snow.css";
@@ -6,49 +6,68 @@ import "./TextEditor.css";
 import { Button } from "@mui/material";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
-import  { auth,db } from "../utilities/Firebase";
+import  { auth, db } from "../utilities/Firebase";
 import jsPDF from "jspdf";
-import  html2pdf  from "html2pdf.js";
+import html2pdf from "html2pdf.js";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-
-
 export const TextEditor = () => {
-
   const history = useHistory();
+  const [state, setState] = useState({ value: "", fileName: "document.pdf" }); // Added fileName state
   const [user] = useAuthState(auth);
-  const [state, setState] = React.useState({ value: null });
   const [docs, loading] = useCollectionData(
-    db.collection(`users/${user?.uid}/documents`).orderBy("createdAt")
+    db.collection(`Docs/${user?.uid}/documents`).orderBy("createdAt")
   );
 
   useEffect(() => {
-    // Initialize the state with an empty content
-    setState({ value: "" });
-    
-    // If you want to load the content of the latest document when available, uncomment the following lines
-    // if (docs && docs.length > 0) {
-    //   setState({ value: docs[docs.length - 1].content });
-    // }
+    setState({ value: "", fileName: "document.pdf" }); // Reset fileName when docs change
   }, [docs]);
+
   const handleChange = (value) => {
-    setState({ value });
+    setState((prev) => ({ ...prev, value }));
   };
- 
 
+  const handleSave = () => {
+    const { value, fileName } = state;
 
+    // Check if a document exists
+    const existingDocument = docs && docs.length > 0 ? docs[docs.length - 1] : null;
+
+    if (existingDocument) {
+      // Update the existing document with new content
+      db.collection(`Docs/${user?.uid}/documents`).doc(existingDocument.id).set({
+        content: value,
+        fileName,
+        updatedAt: new Date(),
+      });
+    } else {
+      // Create a new document in the Firestore collection
+      db.collection(`Docs/${user?.uid}/documents`).add({
+        content: value,
+        fileName,
+        createdAt: new Date(),
+      });
+    }
+  };
+
+  const handleChanges = () => {
+    handleSave();
+    handleSaveAsPDF();
+  };
 
   const handleSaveAsPDF = () => {
-    const { value } = state;
+    const { value, fileName } = state;
     if (value) {
       // Prompt the user for the filename and location
-      const fileName = window.prompt("Enter the file name:", "document.pdf");
-  
+      const newFileName = window.prompt("Enter the file name:", fileName);
+
       // Check if the user canceled or entered an empty name
-      if (fileName === null || fileName.trim() === "") {
+      if (newFileName === null || newFileName.trim() === "") {
         return;
       }
-  
+
+      setState((prev) => ({ ...prev, fileName: newFileName })); // Update fileName
+
       // Create a new jsPDF instance
       const pdf = new jsPDF({
         orientation: 'portrait',
@@ -59,7 +78,7 @@ export const TextEditor = () => {
         marginTop: 20,
         marginBottom: 20,
       });
-  
+
       // Convert HTML content to PDF using html2pdf
       html2pdf(value, {
         margin: 10,
@@ -68,40 +87,18 @@ export const TextEditor = () => {
         callback: (pdf) => {
           // Save the PDF after conversion
           pdf.save(fileName);
-          handleSave();
-          setState({ value: "" });
+          setState({ value: "", fileName: "document.pdf" }); // Reset state
         },
       });
-    }
-    
-    // Navigate back to the main page
-    history.push("/main");
-  };
-  
-  const handleSave = () => {
-    const { value } = state;
 
-    // Check if a document exists
-    const existingDocument = docs && docs.length > 0 ? docs[docs.length - 1] : null;
-
-    if (existingDocument) {
-      // Update the existing document with new content
-      db.collection(`users/${user?.uid}/documents`).doc(existingDocument.id).update({
-        content: value,
-        updatedAt: new Date(),
-      });
-    } else {
-      // Create a new document in the Firestore collection
-      db.collection(`users/${user?.uid}/documents`).add({
-        content: value,
-        createdAt: new Date(),
-      });
+      // Navigate back to the main page
+      history.push("/main");
     }
   };
-  
 
   return (
     <div className="textEditor">
+      <h2>Edit File Name: {state.fileName}</h2>
       <EditorToolbar />
       <ReactQuill
         theme="snow"
@@ -112,7 +109,7 @@ export const TextEditor = () => {
         formats={formats}
         style={{ height: "500px" }}
       />
-      <Button onClick={handleSaveAsPDF}>Save as Pdf</Button>
+      <Button onClick={handleChanges}>Save as Pdf</Button>
     </div>
   );
 };
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Main.css'
 import Card from '../Components/Card'
 import HeaderMain from './HeaderMain'
 import add_sign from '../assets/add_sign.png'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom'
+import { auth, db } from '../utilities/Firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 
 
 function Main() {
 
   const history = useHistory();
+  const [documents, setDocuments] = useState([]);
+  const [user] = useAuthState(auth);
+  const [docs, loading] = useCollectionData(
+    db.collection(`Docs/${user?.uid}/documents`).orderBy("createdAt")
+  );
 
   const writeNote = (event) => {
     event.preventDefault();
     history.push('/writenote');
   }
+
+
+  useEffect(() => {
+    // Update the local state with the documents from Firestore
+    if (!loading && docs) {
+      setDocuments(docs);
+    }
+  }, [loading, docs]);
+
+
+  
   return (
     <div className='main'>
       <HeaderMain />
